d915afe83a0c675ec57d279b4b595e4d7935e1e6
leochen1126 <a1465533500@gmail.com>
2024-05-07 14:02:30 +1000

    create XcodeProject

---
+++ Github-GroupAss3/Github-GroupAss3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Github-GroupAss3
+//
+//  Created by Ming-You Chen on 7/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Github-GroupAss3/Github-GroupAss3/Github_GroupAss3App.swift
@@ -0,0 +1,17 @@
+//
+//  Github_GroupAss3App.swift
+//  Github-GroupAss3
+//
+//  Created by Ming-You Chen on 7/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct Github_GroupAss3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
