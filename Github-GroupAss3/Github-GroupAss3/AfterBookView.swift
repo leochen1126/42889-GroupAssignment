71b5811ca23eb5fd6a5ca2d7e3cbf9142c5d45d2
Gandi <gandi.lu@student.uts.edu.au>
2024-05-12 19:07:01 +1000

    Movie details and Booking done Gandi

---
+++ Github-GroupAss3/Github-GroupAss3/AfterBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AfterBookView.swift
+//  Github-GroupAss3
+//
+//  Created by Admin on 12/5/2024.
+//
+
+import SwiftUI
+
+struct AfterBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AfterBookView()
+}
---
+++ Github-GroupAss3/Github-GroupAss3/AfterBookViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AfterBookViewModel.swift
+//  Github-GroupAss3
+//
+//  Created by Admin on 12/5/2024.
+//
+
+import Foundation
