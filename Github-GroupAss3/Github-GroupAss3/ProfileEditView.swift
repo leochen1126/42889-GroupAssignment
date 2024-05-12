5a0ff243b424b80d36fb82b49fda0bdc1cd1e05d
leochen1126 <a1465533500@gmail.com>
2024-05-12 22:08:48 +1000

    Leo's part + profile edit finished

--- Github-GroupAss3/Github-GroupAss3/ProfileEditView.swift
+++ Github-GroupAss3/Github-GroupAss3/ProfileEditView.swift
@@ -8,8 +8,83 @@
 import SwiftUI
 
 struct ProfileEditView: View {
+    @ObservedObject var viewModel = ProfileEditViewModel()
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                
+                VStack {
+                    Text("Update")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                    TextField("Email", text: $viewModel.email)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                        .onChange(of: viewModel.email) {
+                            viewModel.isEmailValid = viewModel.isValidEmail()
+                        }
+                        .foregroundColor(viewModel.isEmailValid ? .black : .red)
+                    
+                    TextField("Phone Number", text: $viewModel.phoneNumber)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                        .onChange(of: viewModel.phoneNumber) {
+                            viewModel.isPhoneNumberValid = viewModel.isValidPhoneNumber()
+                        }
+                        .foregroundColor(viewModel.isPhoneNumberValid ? .black : .red)
+                    
+                    TextField("Address", text: $viewModel.address)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                        .onChange(of: viewModel.address) {
+                            viewModel.isAddressValid = viewModel.isValidAddress()
+                        }
+                        .foregroundColor(viewModel.isAddressValid ? .black : .red)
+                    
+                    
+                    
+                    Button("Update", action: {
+                                viewModel.onSuccessfulUpdate = {
+                                    presentationMode.wrappedValue.dismiss()
+                                }
+                                viewModel.update()
+                            })
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding()
+                        .disabled(!viewModel.isFormValid())
+                        .alert(isPresented: $viewModel.showAlert) {
+                            Alert(
+                                title: Text("Update Successful"),
+                                message: Text("You have been updated!"),
+                                dismissButton: .default(Text("OK")) {
+                                    // Navigate back to the previous page (e.g., using a presentation mode)
+                                    // You can customize this part based on your navigation setup
+                                }
+                            )
+                        }
+                }
+                .padding()
+                .background(Color.gray.opacity(0.8))
+                .cornerRadius(16)
+                .padding()
+            }
+        }
     }
 }
 
--- Github-GroupAss3/Github-GroupAss3/ProfileEditViewModel.swift
+++ Github-GroupAss3/Github-GroupAss3/ProfileEditViewModel.swift
@@ -6,3 +6,97 @@
 //
 
 import Foundation
+import FirebaseCore
+import FirebaseFirestore
+
+
+
+class ProfileEditViewModel: ObservableObject {
+    @Published var username = UserSettings.shared.username
+    @Published var email = ""
+    @Published var phoneNumber = ""
+    @Published var address = ""
+    var onSuccessfulUpdate: (() -> Void)?
+    
+    // Validation states
+    @Published var isEmailValid = false
+    @Published var isPhoneNumberValid = false
+    @Published var isAddressValid = false
+    @Published var showAlert = false
+    @Published var updateSuccessful = false
+    // Validation functions
+    func isValidUsername() -> Bool {
+        // Add your validation logic for username
+        return !username.isEmpty
+    }
+    
+    func isValidEmail() -> Bool {
+        // Regular expression for validating email address
+        let emailRegex = #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"#
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+    
+    func isValidPhoneNumber() -> Bool {
+        // Add your validation logic for phone number
+        return phoneNumber.count >= 7
+    }
+    
+    func isValidAddress() -> Bool {
+        // Add your validation logic for address
+        return !address.isEmpty
+    }
+    
+    func isFormValid() -> Bool {
+        // Check if all fields are valid
+        return isEmailValid && isPhoneNumberValid && isAddressValid
+    }
+    func update() {
+        // Check if form is valid
+        if isFormValid() {
+            let db = Firestore.firestore()
+            
+            // Get a reference to the "user_info" collection
+            let userCollection = db.collection("user_info")
+            
+            // Create a dictionary with the user's data
+            let userData: [String: Any] = [
+                "userName": username,
+                "email": email,
+                "phoneNumber": phoneNumber,
+                "address": address
+            ]
+            
+            // Query the collection for the document with the matching username
+            userCollection.whereField("userName", isEqualTo: username).getDocuments { (querySnapshot, error) in
+                if let error = error {
+                    print("Error getting documents: \(error)")
+                    self.showAlert = true
+                    self.updateSuccessful = false
+                } else {
+                    for document in querySnapshot!.documents {
+                        // Update the document with the user's data
+                        document.reference.updateData(userData) { error in
+                            if let error = error {
+                                print("Error updating document: \(error)")
+                                self.showAlert = true
+                                self.updateSuccessful = false
+                            } else {
+                                print("Document successfully updated")
+                                self.showAlert = true
+                                self.updateSuccessful = true
+                                DispatchQueue.main.async {
+                                    self.onSuccessfulUpdate?()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            print("Form is not valid")
+            self.showAlert = true
+            self.updateSuccessful = false
+        }
+    }
+}
