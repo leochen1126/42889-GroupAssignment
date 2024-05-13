f91700dcf1b98cce1ae80641700eb2e1e7d29b7f
leochen1126 <a1465533500@gmail.com>
2024-05-13 19:06:22 +1000

    leo's part

--- Github-GroupAss3/Github-GroupAss3/ProfileView.swift
+++ Github-GroupAss3/Github-GroupAss3/ProfileView.swift
@@ -2,74 +2,82 @@
 //  ProfileView.swift
 //  Github-GroupAss3
 //
-//  Created by Ming-You Chen on 9/5/2024.
+//  Created by Ming-You Chen on 12/5/2024.
 //
+
 import SwiftUI
 
 struct ProfileView: View {
     @ObservedObject var viewModel = ProfileViewModel()
     @Environment(\.presentationMode) var presentationMode
-
     var body: some View {
         NavigationView {
             ZStack {
                 Color.black.edgesIgnoringSafeArea(.all)
                 VStack {
-                    userInfoSection
-                    actionButtons
+                    VStack {
+                        Text(viewModel.username)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                        Text("Email:\(viewModel.email)")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                        Text("Address:\(viewModel.address)")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                        Text("Phone:\(viewModel.phone)")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                        
+                        
+                        HStack {
+                            Spacer()
+                            
+                            NavigationLink(destination: ProfileEditView()) {
+                                Text("Edit")
+                                    .foregroundColor(.white)
+                                    .padding()
+                            }
+                            .background(Color.blue)
+                            .cornerRadius(8)
+                            Spacer()
+                            Button(action: viewModel.logout, label: {
+                                Text("LogOut")
+                                    .foregroundColor(.white)
+                                    .padding()
+                            })
+                            .background(Color.blue)
+                            .cornerRadius(8)
+                            
+                            Spacer()
+                        }
+                        .padding()
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.8))
+                    .cornerRadius(16)
+                    .padding()
                 }
-                .onAppear(perform: viewModel.fetchUserDetails)
+                .onAppear{
+                    viewModel.fetchUserDetails()
+                    // Set the dismissal handler
+                    viewModel.handleDismissal = {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }
             }
         }
-    }
-
-    private var userInfoSection: some View {
-        VStack {
-            userInfoText("Username: \(viewModel.username)", font: .largeTitle)
-            userInfoText("Email: \(viewModel.email)")
-            userInfoText("Address: \(viewModel.address)")
-            userInfoText("Phone: \(viewModel.phone)")
-        }
-        .padding()
-        .background(Color.gray.opacity(0.8))
-        .cornerRadius(16)
-        .padding()
-    }
-
-    private func userInfoText(_ text: String, font: Font = .title) -> some View {
-        Text(text)
-            .font(font)
-            .fontWeight(.bold)
-            .foregroundColor(.white)
-            .padding()
-    }
-
-    private var actionButtons: some View {
-        HStack {
-            Spacer()
-            NavigationLink(destination: ProfileEditView()) {
-                actionButtonLabel("Edit")
-            }
-            Spacer()
-            Button(action: viewModel.logout) {
-                actionButtonLabel("LogOut")
-            }
-            Spacer()
-        }
-        .padding()
-    }
-
-    private func actionButtonLabel(_ text: String) -> some View {
-        Text(text)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .cornerRadius(8)
     }
 }
 
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
-    }
+#Preview {
+    ProfileView()
 }
