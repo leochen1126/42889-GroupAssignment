a11663fcec4e61cafcdb48ca996a2d34b9677126
Anna Huang <yu-an.huang@student.uts.edu.au>
2024-05-14 05:19:48 +1000

    Seats selection

---
+++ Github-GroupAss3/Github-GroupAss3/SeatsView.swift
@@ -0,0 +1,18 @@
+//
+//  SeatsView.swift
+//  Github-GroupAss3
+//
+//  Created by Anna Huang on 14/5/2024.
+//
+
+import SwiftUI
+
+struct SeatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeatsView()
+}
---
+++ Github-GroupAss3/Github-GroupAss3/SeatsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SeatsViewModel.swift
+//  Github-GroupAss3
+//
+//  Created by Anna Huang on 14/5/2024.
+//
+
+import Foundation
